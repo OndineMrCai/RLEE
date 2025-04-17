a08dce40b47cc3d5c3039c04c6d16d523f63dbd4
OndineMrCai <s56cai@uwaterloo.ca>
2025-04-16 23:21:02 -0400

    change batch size

--- scrpits/train/train.sh
+++ scrpits/train/train.sh
@@ -20,7 +20,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.optim.lr=5e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=8 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=16384 \
     actor_rollout_ref.actor.use_kl_loss=True \
