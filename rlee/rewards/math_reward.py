b6c89d8f8b873d0b675bee7ead3535a1e77d2e85
OndineMrCai <s56cai@uwaterloo.ca>
2025-04-18 01:58:54 -0400

    fix extract_solution

--- rlee/rewards/math_reward.py
+++ rlee/rewards/math_reward.py
@@ -68,7 +68,9 @@ def extract_solution(solution_str: str) -> Tuple[Optional[str], str]:
         Tuple containing (extracted_answer, processed_string)
     """
     # Split response to isolate assistant output
-    if "Assistant:" in solution_str:
+    if "<｜Assistant｜>" in solution_str:
+        processed_str = solution_str.split("<｜Assistant｜>", 1)[1]
+    elif "Assistant:" in solution_str:
         processed_str = solution_str.split("Assistant:", 1)[1]
     elif "<|im_start|>assistant" in solution_str:
         processed_str = solution_str.split("<|im_start|>assistant", 1)[1]
